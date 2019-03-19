436eff68268cfc8089c150b445cd94730dea7887
michelletm <michellemirek@gmail.com>
2019-03-18 17:47:59 -0700

    additional start files added

---
+++ keys.js
@@ -0,0 +1,9 @@
+console.log('this is loaded');
+
+exports.spotify = {
+  id: process.env.SPOTIFY_ID,
+  secret: process.env.SPOTIFY_SECRET
+};
+
+http://www.omdbapi.com/?i=tt3896198&apikey=63f442ef
+
