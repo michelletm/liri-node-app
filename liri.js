7e97a4aa7d0389fd776e69fb61ae20ea2e6abd7c
michelletm <michellemirek@gmail.com>
2019-03-21 14:40:25 -0700

    final stages and readme

--- liri.js
+++ liri.js
@@ -8,20 +8,22 @@ const userInput = process.argv[2].toLowerCase();
 let userQuery = process.argv[3];
 
 
-
 //////CONCERT-THIS///////////////////////
 
 var concertThis = function(userQuery) {
  
   axios.get(`https://rest.bandsintown.com/artists/${userQuery}/events?app_id=${keys.BAND_ID}`)
   .then( function(response) {
+    
     let data = "";
+    //console.log(response.data);
     
     console.log(chalk.magenta("Tour/Venue: " + response.data[0].venue.name));
     console.log("Location: " + response.data[0].venue.city);
     console.log("Date: " + (moment(response.data[0].datetime).format("MM/DD/YYYY")));
     data = `Tour/Venue: ${response.data[0].venue.name}\nLocation: ${response.data[0].venue.city}\nDate: ${(moment(response.data[0].datetime).format("MM/DD/YYYY"))}\n\n`
     output(data);
+  
   });
 };
 
@@ -33,9 +35,14 @@ var spotifyThis = function(userQuery) {
   const Spotify = require("node-spotify-api");
   const spotify = new Spotify(keys.SPOTIFY_KEY);
 
+  if (!userQuery) {
+    userQuery = "The Sign";
+  }
+
   spotify.search({type: 'track', query: userQuery}, function(err, response) {
-    let data = ""
-    
+     
+    let data = "";
+
     if (err) {
       return console.log(chalk.red('Error Occurred: ' + err));
     }
@@ -44,14 +51,11 @@ var spotifyThis = function(userQuery) {
     console.log("Album: " + response.tracks.items[0].album.name);
     console.log("Artist: " + response.tracks.items[0].artists[0].name);
     console.log("Preview: " + response.tracks.items[0].preview_url);
-      
-  if (userQuery === "") {
-    userQuery = "The Sign";
-  }
+
     data = `Song: ${response.tracks.items[0].name}\nArtist: ${response.tracks.items[0].artists[0].name}\n\n`
     output(data);
-  }
-  );
+  
+  });
 
 };
 
@@ -59,9 +63,15 @@ var spotifyThis = function(userQuery) {
 ////////MOVIE-THIS/////////////////
 
 var movieThis = function(userQuery) {
+
+  if(!userQuery) {
+    userQuery = "Mr. Nobody";
+  }
+  
   axios.get(`http://www.omdbapi.com/?t=${userQuery}&y=&plot=short&apikey=${keys.OMDB_KEY}`).then(
     function(response) {
-     let data = "";
+      
+      let data = "";
 
       console.log(chalk.blueBright("Movie Title: " + response.data.Title));
       console.log("Released: " + response.data.Year);
@@ -72,20 +82,19 @@ var movieThis = function(userQuery) {
       console.log("Plot: " + response.data.Plot);
       console.log("Actors: " + response.data.Actors);
     
-    if(userInput === "") {
-      userQuery = "Mr. Nobody";
-      
-    }
     data = `Movie Title: ${response.data.Title}\nReleased: ${response.data.Year}\nActors: ${response.data.Actors}\n\n`;
     output(data);
 
   });
 };
 
+
 ////DO WHAT IT SAYS/////////////////////
 
 var doWhat = function() {
+  
   let data = "";
+  
   fs.readFile("random.txt", 'utf-8', function(err, response) {
 
     var result = response.split(",");
@@ -94,8 +103,8 @@ var doWhat = function() {
       if (err) {
         console.log(chalk.red('Error Occurred: ' + err));
       }
-    
       if(result[0] === "concert-this") {
+        console.log(result[1]);
         concertThis(result[1]);
         //console.log(result[1]);
       }
@@ -105,18 +114,15 @@ var doWhat = function() {
       if(result[0] === "movie-this") {
         movieThis(result[1]);
       }
-      // if (err) {
-      //   console.log(chalk.red('Error Occurred: ' + err));
-      // }
-     
-      data =`${result[1]}\n\n`;
+    
       output(data);
+  
   });
   
 };
 
 
-////////OUTPUT//////
+////////BONUS-OUTPUT//////
 
 var output = function(data) {
 
@@ -146,7 +152,6 @@ switch(userInput) {
     break;
   case "do-what-it-says":
     doWhat();
-    //break;
-   
-};
+};  
+
 
